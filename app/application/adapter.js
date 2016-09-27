09ecd715397b5694ed672876c868e0a5264c5edc
alex-braun <braunacb@gmail.com>
2016-09-27 06:08:29 -0400

    Initial Commit.

---
+++ app/application/adapter.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+import ActiveModelAdapter from 'active-model-adapter';
+import ENV from 'ga-wdi-boston.ember-auth/config/environment'; //deployment step
+
+export default ActiveModelAdapter.extend({
+  host: ENV.apiHost, //deployment step
+  auth: Ember.inject.service(),
+
+  headers: Ember.computed('auth.credentials.token', {
+    get () {
+      let headers = {};
+      const token = this.get('auth.credentials.token');
+      if (token) {
+        headers.Authorization = `Token token=${token}`;
+      }
+
+      return headers;
+    },
+  }),
+});
